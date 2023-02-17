9b0fb04a838b9c15578f4b62a06e315c2547e605
Martin <martin_couadeau1@hotmail.com>
2023-02-17 06:03:52 -0300

    esqueleto de Front creado

---
+++ client/src/Redux/Reducer/index.js
@@ -0,0 +1,13 @@
+
+
+
+export default function rootReducer(state=initialState, action) {
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
